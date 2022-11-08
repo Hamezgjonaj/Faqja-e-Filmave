aa5d2a682c15c61085d8a4970750f0406e08b208
Hamezgjonaj <hanigjonaj@gmail.com>
2022-11-08 17:44:19 +0100

    the installation of two libraries to help me use the video and youtube video player

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,12 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA }      from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { MoviesComponent } from './movies.component';
+import { NavbarComponent } from './nav-bar/nav-bar.component';
+import { MovieTypeComponent } from './movie-type/movie-type.component';
+import { BannerComponent } from './banner/banner.component';
+import { YouTubePlayerModule } from '@angular/youtube-player';
+import { MoviesRoutingModule } from './movies-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MoviesComponent,
+    NavbarComponent,
+    MovieTypeComponent,
+    BannerComponent
+  ],
+
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+
   imports: [
-    CommonModule
+    CommonModule,
+    YouTubePlayerModule,
+    MoviesRoutingModule
   ]
 })
 export class MoviesModule { }
